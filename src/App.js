4f17e57dff6ffc805b69b3509cc665c82fd72c5a
Kristina <107880739+NinjaGexly@users.noreply.github.com>
2023-02-11 21:09:00 +0300

    created basic file structure, added project images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
+import Search from "./Search";
+import background from "./img/Bg.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        width: "100vw",
+        height: "100vh",
+      }}
+      className="App"
+    >
+      <div className="container">
+        <Search />
+        <Weather />
+      </div>
     </div>
   );
 }
